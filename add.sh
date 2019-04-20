f40373a108d69a266b767d3f92cc1b2b7794da60
lushchan <mluschan@gmail.com>
2019-04-20 10:38:11 +0000

    some fixes

--- add.sh
+++ add.sh
@@ -1,21 +1,29 @@
-#!/bin/bash
-echo "Install stream rotator to /home/ftpaccess/domain.com/www/r33x"
+#!/bin/bash -e
+cat /dev/null > ./domains
+echo "Install stream rotator"
 echo "edit file "domains" before installation"
 touch ./domains
 vim ./domains
-echo "User name:"
+echo "User name:(default: ftpaccess)"
 read -e username
+if [[ -z $username ]]; then
+username=ftpaccess
+fi
 echo "Installation directory: "
 read -e workdir
-echo "Continue? (y/n)"
-read -e run
-if [ "$run" == n ] ; then
-exit
-else
 cat ./domains | while read domain
 do
-mkdir -p /home/$username/$domain/www/$workdir
+mkdir /home/$username/$domain/www/$workdir
 chmod 777 /home/$username/$domain/www/$workdir
+DIR=/home/$username/$domain/www
+DIR2=/home/$username/$domain/html
+DIR3=/home/$username/$domain/public_html
+if [ -d $DIR ] || [ -d $DIR2 ] || [ -d $DIR3 ] ; then
+   echo "All looks fine"
+else
+   echo "Unknown document root"
+exit 
+fi
 dbname=r3`echo $domain | cut -d / -f 4| sed -e 's/-/_/g'|sed 's|\.||g'`
 dbuser=r3u`echo $domain | cut -d / -f 4|cut -c 1-13 | sed 's|-|_|g'|sed 's|\.||g'`
 dbpass=`pwgen 12 1`
@@ -30,8 +38,6 @@ echo "dbName $dbname"
 echo "dbUser: $dbuser"
 echo "dbPassword: $dbpass"
 echo -e "* * * * * cd /home/$username/$domain/www/$workdir; /usr/bin/php -q cron.php 1>/dev/null 2>/dev/null \n" >> /var/spool/cron/ftpaccess
-done
 echo "DONE! Go to URL to complete installation. Dont forget about database credentials. Check some lines before!!!"
-fi
-cat /dev/null > ./domains
+done
 exit
