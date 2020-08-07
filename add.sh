76fd2dc70c994d10dfa3c5a9caf594a2d76acc52
lmr <41105878+lushchan@users.noreply.github.com>
2020-08-07 19:25:40 +0300

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,44 +1,52 @@
 #!/bin/bash -e
-cat /dev/null > ./domains
-echo "Install stream rotator"
-echo "edit file "domains" before installation"
-#touch ./domains
-vim ./domains
-echo "User name:(default: ftpaccess)"
-read -e username
-if [[ -z $username ]]; then
-  username=ftpaccess
+#logging
+##timestampformat
+tstamp=$(date +%m/%d/%Y-%H:%M:%S)
+##credentials
+creds=./creds.txt
+#inputfile
+ifile=./r33x.txt
+phppath=`which php$phpver`
+phppathdef=`which php`
+vim $ifile
+echo "Choice a php version:(default: $phppathdef)"
+read -e phpver
+if [[ -z phpver ]]; then
+  phppath=$phppathdef
 fi
-echo "Installation directory: "
-read -e workdir
-echo "Document root:(default: www)"
-read -e $docroot
-if [[ -z $docroot ]]; then
-  docroot=www
-fi
-echo "Continue? (y/n)"
-read -e run
-if [ "$run" == n ] ; then
-exit
-fi
-  cat ./domains | while read domain
+phppath=`which php$phpver`
+#download section
+wget http://streamscripts.com/rotator/install.zip
+unzip install.zip
+#
+mysqlloc=`which mysql`
+mysqldumploc=`which mysqldump`
+convertloc=`which convert`
+cat $ifile | while read domain
+docroot=$(grep -r "$domain" /etc/nginx/ | grep root | awk '{print $(NF-1), $NF}'  | sed 's/root //g;s/;//g' | sed 's/^[ \t]*//g;s/[ \t]*$//g;s|/$||g' | sort -u | head -n 1)
+r33xroot=$docroot/r33x
+username=`stat -c "%U" $docroot`
 do
-  mkdir /home/$username/$domain/$docroot/$workdir
-  chmod 777 /home/$username/$domain/$docroot/$workdir
+  mkdir $r33xroot
+  chmod 777 $r33xroot
+  cp install.php $r33xroot/install.php
   dbname=r3`echo $domain | cut -d / -f 4| sed -e 's/-/_/g'|sed 's|\.||g'`
   dbuser=r3u`echo $domain | cut -d / -f 4|cut -c 1-13 | sed 's|-|_|g'|sed 's|\.||g'`
-  dbpass=`pwgen 12 1`
+  dbpass=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13`
   mysql -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
   mysql -e "CREATE USER ${dbuser}@localhost IDENTIFIED BY '${dbpass}';"
   mysql -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${dbuser}'@'localhost';"
   mysql -e "FLUSH PRIVILEGES;"
-  wget http://streamscripts.com/rotator/install55.zip -P /home/$username/$domain/$docroot/$workdir
-  unzip /home/$username/$domain/$docroot/$workdir/install55.zip -d /home/$username/$domain/$docroot/$workdir
-  echo "dbHost: localhost"
-  echo "dbName $dbname"
-  echo "dbUser: $dbuser"
-  echo "dbPassword: $dbpass"
-  echo -e "* * * * * cd /home/$username/$domain/$docroot/$workdir; /usr/bin/php -q cron.php 1>/dev/null 2>/dev/null \n" >> /var/spool/cron/$username
+  echo "MySql host: localhost"
+  echo "MySql user: $dbuser"
+  echo "MySql password: $dbpass"
+  echo "MySql database: $dbname"
+  echo "mysql utility location: $mysqlloc"
+  echo "mysqldump utility location: $mysqldumploc"
+  echo "IM convert utility location: $convertloc"
+  echo "PHP location: $phppath"
+  echo "Script directory: ."
+  echo -e "* * * * * cd $r33xroot; $phppath -q cron.php 1>/dev/null 2>/dev/null \n" >> /var/spool/cron/$username
   echo "DONE! Go to URL to complete installation. Dont forget about database credentials. Check some lines before!!!"
 done
 exit
